23502d6209ca0c0fd60cb2de009d18b978cc392f
LemanAlizadeh <95859151+LemanAlizadeh@users.noreply.github.com>
2023-10-05 11:39:05 +0400

    sidebar

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,8 +1,34 @@
+import React from "react";
+import {
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+  BsJustify,
+} from "react-icons/bs";
 
-function Header() {
+function Header({ OpenSidebar }) {
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div className="menu-icon">
+        <BsJustify className="icon" onClick={OpenSidebar} />
+      </div>
+      <div className="header-left">
+        <BsSearch className="icon" />
+      </div>
+      <div className="header-right">
+        <a href="#">
+          <BsFillBellFill className="icon" />
+        </a>
+        <a href="#">
+          <BsFillEnvelopeFill className="icon" />
+        </a>
+        <a href="#">
+          <BsPersonCircle className="icon" />
+        </a>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
